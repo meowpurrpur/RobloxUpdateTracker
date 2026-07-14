69823fc63c0cc5026bb55be4b2990284dd3b5e93
theo <contact@imtheo.lol>
2026-07-14 21:18:17 +0100

    comment out testing url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
-//export const CLIENTSETTINGS_BASE = "https://clientsettings.roblox.com";
-export const CLIENTSETTINGS_BASE = "http://localhost:7722";
+export const CLIENTSETTINGS_BASE = "https://clientsettings.roblox.com";
+//export const CLIENTSETTINGS_BASE = "http://localhost:7722";
+
 export const RDD_BASE = "https://rdd.latte.to"
 export const ROBLOX_CHANNELS = ["LIVE", "ZBeta"] as const;
 
