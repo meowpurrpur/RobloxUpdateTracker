ce2991dd033ef6c561d6cbc43bfb7aab6d4c6d54
theo <contact@imtheo.lol>
2026-07-22 00:35:05 +0100

    fix monitoring

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,8 +1,10 @@
 import { Events } from "discord.js";
+import { startMonitoring } from "../monitoring";
 
 export const name = Events.ClientReady;
 export const once = true;
 
 export async function execute(...args: any[]) {
   console.log("Logged into Discord bot!");
+  startMonitoring();
 }
